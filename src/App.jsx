920587c72193cf54a41a1e225d2588c08ae94175
wendybarrantes <wenbarrantes06@gmail.com>
2024-09-02 21:29:13 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./estilos/estilos_practica.css"
+
+function App() {
+
+  return (
+    <>
+     <nav><a href="">inicio</a>
+     <a href="">contacto</a>
+     <a href="">nosotros</a>
+     <a href="">productos</a>
+     <a href="">precio</a></nav> 
+    </>
+  )
+}
+
+export default App
